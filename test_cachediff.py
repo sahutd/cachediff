4ad20bf88337574156f3e215e3145e86ce3303dd
Saimadhav Heblikar <saimadhavheblikar@gmail.com>
2015-10-14 20:25:19 +0530

    added test for assembly_instruction

--- test_cachediff.py
+++ test_cachediff.py
@@ -1,12 +1,14 @@
 import unittest
+import cachediff
 
 
-class TestFile(unittest.TestCase):
-    pass
+class TestAssemblyInstruction(unittest.TestCase):
+    def test_simple(self):
+        line = '4005d6:       48 83 c4 08             add    $0x8,%rsp'
+        ai = cachediff.AssemblyLine(line)
+        self.assertEqual(ai.get_virtual_address(), 0x4005d6)
 
 
-class TestLineDetection(unittest.TestCase):
-    pass
 
 if __name__ == '__main__':
     unittest.main()
